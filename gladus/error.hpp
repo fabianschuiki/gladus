812f54b5098e368cb5e34133a261ce30f9a67127
Fabian Schuiki <fschuiki@student.ethz.ch>
2013-12-30 10:08:28 +0100

    added error.hpp
    The error.hpp header contains exception handling code. It defines a base gladus::error class which derives from std::exception. Based on this an opengl_error is defined that captures an error returned from glGetError(); and runtime_error which captures a nested chain of runtime_errors and one underlying opengl_error.
    
    Additional functions are provided that help to check for OpenGL errors, or throw an exception if an OpenGL error is encountered. Two macros, if_opengl_error and incase_opengl_error, are provided to shorten OpenGL error handling code. The error checking functions may be converted into a nop via a preprocessor define.

---
+++ gladus/error.hpp
@@ -0,0 +1,85 @@
+/* Copyright (c) 2013 Fabian Schuiki */
+#pragma once
+#include <stdexcept>
+#include <string>
+#define GLADUS_HAS_ERROR
+
+namespace gladus {
+
+struct error : public std::exception {};
+
+struct opengl_error : public error
+{
+	GLenum ec;
+
+	opengl_error(): ec(GL_NO_ERROR) {}
+	opengl_error(GLenum ec): ec(ec) {}
+
+	virtual const char* what() const throw()
+	{
+		switch (ec) {
+			case GL_INVALID_ENUM: return "invalid opengl enum";
+			case GL_INVALID_VALUE: return "invalid opengl value";
+			case GL_INVALID_OPERATION: return "invalid opengl operation";
+			case GL_INVALID_FRAMEBUFFER_OPERATION: return "invalid opengl framebuffer operation";
+			case GL_OUT_OF_MEMORY: return "opengl out of memory";
+			default: return "unknown opengl error"; break;
+		}
+	}
+};
+
+struct runtime_error : public error
+{
+	opengl_error underlying_gl_error;
+	std::string message;
+
+	runtime_error(): message("") {}
+	runtime_error(const char* msg): message(msg) {}
+	runtime_error(const char* msg, const opengl_error& err): message(msg), underlying_gl_error(err) {}
+	runtime_error(const char* msg, const runtime_error& err): message(msg), underlying_gl_error(err.underlying_gl_error) { message += "; after " + err.message; }
+	virtual ~runtime_error() throw() {}
+
+	virtual const char* what() const throw()
+	{
+		std::string composed = "gladus: ";
+		composed += (message.empty() ? "unknown error" : message);
+		if (underlying_gl_error.ec != GL_NO_ERROR) {
+			composed += " (";
+			composed += underlying_gl_error.what();
+			composed += ')';
+		}
+		return composed.c_str();
+	}
+};
+
+#ifdef GLADUS_DONT_CHECK_GL_ERRORS
+#	define clear_opengl_error()
+#	define check_opengl_error(err) false
+#	define throw_on_opengl_error()
+#	define if_opengl_error(err) opengl_error err; if (false)
+#	define incase_opengl_error(err) opengl_error err; if (false) switch(GL_NO_ERROR)
+#else
+inline void clear_opengl_error() { glGetError(); }
+
+inline bool check_opengl_error(opengl_error& err)
+{
+	GLenum e = glGetError();
+	if (e != GL_NO_ERROR) {
+		err = opengl_error(e);
+		return true;
+	}
+	return false;
+}
+
+inline void throw_on_opengl_error()
+{
+	opengl_error err;
+	if (check_opengl_error(err))
+		throw err;
+}
+
+#define if_opengl_error(err) opengl_error err; if (check_opengl_error(err))
+#define incase_opengl_error(err) opengl_error err; if (check_opengl_error(err)) switch (err.ec)
+#endif
+
+} // namespace gladus
